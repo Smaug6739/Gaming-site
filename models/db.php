113fabc20ffa10cadaacd18c35384ef1a43208da
SMAUG <59796136+SmaugDev@users.noreply.github.com>
2020-07-18 11:15:29 +0200

    Update db.php

--- models/db.php
+++ models/db.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = new PDO('mysql:host=localhost;dbname=frenngyp_fgf_base_donnee', 'frenngyp_admin', 'Io73uh96/&*');
+$db = new PDO('mysql:host=localhost;dbname=frenngyp_fgf_base_donnee', 'root', '');
